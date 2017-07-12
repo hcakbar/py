35fabb6753379b5fae0abc1a8c3771d99977dea8
hcakbar <hc.akbar@gmail.com>
2017-07-12 19:34:14 -0400

    3 changed files, plus 2 migrations

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import redirect, render
+from lists.models import Item, List
+from django.db import models
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
+
+class List(models.Model):
+    pass
+
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
